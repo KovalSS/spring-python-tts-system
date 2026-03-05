ee8ccb5f51ab98df5c08a3e06ca0bca25c0a4054
SerhiyKoval <kovalss2006@gmail.com>
2026-03-05 09:06:42 +0200

    feat(python): add MinIO support for text downloading and audio uploading

--- worker-python/config.py
+++ worker-python/config.py
@@ -2,3 +2,9 @@
 RABBITMQ_URL = "amqp://user:password@localhost:5672/"
 INPUT_QUEUE = "tts_requests"   # Java writes here, Python reads
 OUTPUT_QUEUE = "tts_responses" # Python writes here, Java reads
+
+MINIO_URL = "localhost:9000"
+MINIO_ACCESS_KEY = "admin"
+MINIO_SECRET_KEY = "password123"
+TEXT_BUCKET = "text-bucket"
+SPEECH_BUCKET = "speech-bucket"
